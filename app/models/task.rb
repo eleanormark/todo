bb51418de76ad6400fedcfa7f1af587089ef9fc3
eleanormark <eleanormark@gmail.com>
2013-11-13 17:04:36 -0800

    make public methods public

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,16 +3,17 @@ class Task < ActiveRecord::Base
   after_initialize :set_not_done
   belongs_to :list
 
-  private
-  def set_not_done
-    self.done ||= false
-  end
-
   def self.incomplete
     Task.where(:done => false)
   end
 
   def self.complete
     Task.where(:done => true)
+  end
+
+private
+  
+  def set_not_done
+    self.done ||= false
   end
 end
